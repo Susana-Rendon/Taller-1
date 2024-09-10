2819a8d5716a1dce48a20dbae0bbf0ec46118d14
Susana-Rendon <susanarb30@gmail.com>
2024-09-10 18:24:19 -0500

    Add files via upload

--- Taller_eva_operadores/6.Notas.py
+++ Taller_eva_operadores/6.Notas.py
@@ -4,8 +4,12 @@ nota3 = float(input("Ingrese la nota del cuestionario 1: "))
 nota4 = float(input("Ingrese la nota del cuestionario 2: "))
 nota5 = float(input("Ingrese la nota del proyecto de aula: "))
 
-nota_def = nota1 * 0.2 + nota2 * 0.15 + nota3 * 0.22 + nota4 * 0.1 + nota5 * 0.33
+if 1 <= nota1 <= 5 and 1 <= nota2 <= 5 and 1 <= nota3 <= 5 and 1 <= nota4 <= 5 and 1 <= nota5 <= 5:
+    nota_def = nota1 * 0.2 + nota2 * 0.15 + nota3 * 0.22 + nota4 * 0.1 + nota5 * 0.33
+    print(f"La nota definitiva del curso de fundamentos de algoritmos es: {nota_def:.2f}")
+else:
+    print("ingreso un valor incorrecto")
 
-print(f"La nota definitiva del curso de fundamentos de algoritmos es: {nota_def:.2f}")
 
-if 
+
+
