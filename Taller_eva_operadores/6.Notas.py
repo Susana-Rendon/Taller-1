86e71647bf11bdaac348ddb038466b94e9720a92
Susana-Rendon <susanarb30@gmail.com>
2024-09-13 18:47:31 -0500

    Add files via upload

--- Taller_eva_operadores/6.Notas.py
+++ Taller_eva_operadores/6.Notas.py
@@ -7,8 +7,25 @@ nota5 = float(input("Ingrese la nota del proyecto de aula: "))
 if 1 <= nota1 <= 5 and 1 <= nota2 <= 5 and 1 <= nota3 <= 5 and 1 <= nota4 <= 5 and 1 <= nota5 <= 5:
     nota_def = nota1 * 0.2 + nota2 * 0.15 + nota3 * 0.22 + nota4 * 0.1 + nota5 * 0.33
     print(f"La nota definitiva del curso de fundamentos de algoritmos es: {nota_def:.2f}")
+
+    if 1 <= nota_def <= 2.9:
+        nota_def = "Su nota es insuficiente"
+    elif 3 <= nota_def <= 3.9:
+        nota_def = "Su nota es regular"
+    elif 4 <= nota_def < 4.5:
+        nota_def = "Su nota es buena"
+    elif 4.6 < nota_def <= 5:
+        nota_def = "Su nota es excelente"
+    else:
+        print("ingreso un valor incorrecto")
+
+    print(nota_def)
 else:
-    print("ingreso un valor incorrecto")
+    print("Ingresó un valor incorrecto en las notas. Las notas deben estar entre 1 y 5.")
+
+
+
+
 
 
 
